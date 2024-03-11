c1e60356da920cf93d9ea6e258b2ea3b808a96af
Amit-softclique <royamit833@gmail.com>
2024-03-11 14:18:12 +0530

    react fabric feature add

--- src/component/Home.tsx
+++ src/component/Home.tsx
@@ -2,19 +2,14 @@ import React, { useState, useEffect, useRef } from "react";
 import { fabric } from "fabric";
 
 const Home = () => {
-  const [count, setCount] = useState(0);
-  const [counterStart, setCounterStart] = useState(false);
-  const [canvas, setCanvas] = useState();
   const canvasRef = useRef(null);
 
   useEffect(() => {
     const canvas = new fabric.Canvas(canvasRef.current, {
-      // Add any Fabric.js options here
       width: 800,
       height: 800,
     });
 
-    // Example: Adding a circle to the canvas
     const circle = new fabric.Circle({
       radius: 20,
       fill: 'red',
@@ -23,32 +18,10 @@ const Home = () => {
     });
     canvas.add(circle);
 
-    // Clean up Fabric.js instance on unmount
     return () => {
       canvas.dispose();
     };
   }, []);
-
-  const initCanvas = () =>
-    new fabric.Canvas("canvas", {
-      height: 800,
-      width: 800,
-      backgroundColor: "pink",
-    });
-
-  useEffect(() => {
-    let timer: any;
-    if (counterStart) {
-      timer = setInterval(() => {
-        setCount((prev) => prev + 1);
-      }, 1000);
-    }
-
-    return () => {
-      clearInterval(timer);
-    };
-  }, [counterStart]);
-
 
   return (
     <>
